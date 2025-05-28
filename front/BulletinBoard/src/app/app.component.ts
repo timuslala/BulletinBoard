f30e85bc5acf0638b3339af62304414fbe0172ef
MATIK0582 <matik0582@gmail.com>
2025-05-28 16:55:43 +0200

    File structure cleanup, basic header component

--- front/BulletinBoard/src/app/app.component.ts
+++ front/BulletinBoard/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-header [title]="pageTitle()" />
+    <main>
+      <router-outlet />
+    </main>
+  `,
+  styles: [
+    `
+      main {
+        padding: 16px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'BulletinBoard';
+  pageTitle = signal('BulletinBoard');
 }

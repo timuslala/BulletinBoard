9fb067f899c4bfe4dd0362c332a9328bfffb322a
MATIK0582 <matik0582@gmail.com>
2025-06-11 14:10:25 +0200

    CORS disabled fix

--- back/src/main/java/com/back/SecurityConfig.java
+++ back/src/main/java/com/back/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
             .cors(cors -> cors
             .configurationSource(request -> {
                 var config = new org.springframework.web.cors.CorsConfiguration();
-                config.setAllowedOrigins(java.util.List.of("*"));
+                config.setAllowedOrigins(java.util.List.of("http://localhost:4200"));
                 config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 config.setAllowedHeaders(java.util.List.of("*"));
                 config.setAllowCredentials(true);
